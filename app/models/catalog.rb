5e399e1c57ebac687260330593f9756530e582d5
Murad Yusufov <flood4life@gmail.com>
2015-02-25 17:38:48 +0300

    Added uniqueness validations for Catalog and Product models.

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -1,3 +1,6 @@
 class Catalog < ActiveRecord::Base
   has_many :products
+
+  validates :name, uniqueness: true
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :catalog
   has_many :reviews
+
+  validates :name, uniqueness: {scope: :catalog_id, message: "Name should be unique in category"}
 end
