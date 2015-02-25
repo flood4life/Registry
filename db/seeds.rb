f10031ea575d9a19c9e355435f33d9a208ddfff5
Murad Yusufov <flood4life@gmail.com>
2015-02-25 17:39:05 +0300

    Added seed data for database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+books = Catalog.create name: 'Books'
+movies =  Catalog.create name: 'Movies'
+
+
+books.products.create name: 'Clockwork Orange', description: 'Anti-utopia.'
+books.products.create name: 'Wee Free Men', description: 'Fantasy, Comedy.'
+
+movies.products.create name: 'Clockwork Orange', description: 'Anti-utopia.'
+movies.products.create name: 'Pulp Fiction', description: 'Criminal, Comedy.'
+movies.products.create name: 'Pulp Fiction', description: 'Second Fiction.'
+
+
+Product.find(1).reviews.create message: 'Language is quite unique.'
+Product.find(2).reviews.create message: 'Hilarious book.'
+Product.find(3).reviews.create message: 'Epic dialogs.'
+
+
