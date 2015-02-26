794acfdc0bb02b9c969b8373c05e7ab34c333248
Murad Yusufov <flood4life@gmail.com>
2015-02-26 10:54:51 +0300

    Change Add Review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,30 +2,23 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :set_product
 
-  # GET /reviews
-  # GET /reviews.json
   def index
     @reviews = @product.reviews.all
   end
 
-  # GET /reviews/1
-  # GET /reviews/1.json
   def show
   end
 
-  # GET /reviews/new
   def new
     @review = Review.new
   end
 
-  # GET /reviews/1/edit
   def edit
   end
 
-  # POST /reviews
-  # POST /reviews.json
   def create
     @review = Review.new(review_params)
+    @product.reviews << @review
 
     respond_to do |format|
       if @review.save
@@ -38,8 +31,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
   def update
     respond_to do |format|
       if @review.update(review_params)
@@ -52,8 +43,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
   def destroy
     @review.destroy
     respond_to do |format|
@@ -63,7 +52,6 @@ class ReviewsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
     end
@@ -72,8 +60,7 @@ class ReviewsController < ApplicationController
       @product = Product.find params[:product_id]
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:message, :product_id)
+      params.require(:review).permit(:message)
     end
 end
