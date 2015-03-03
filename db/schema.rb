75a644608baccc5aa407d606620c39520acc0d07
Murad Yusufov <flood4life@gmail.com>
2015-03-03 14:40:50 +0300

    Add forgotten schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303110743) do
+ActiveRecord::Schema.define(version: 20150303113620) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",              default: "", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150303110743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.boolean  "is_pending"
   end
 
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id"
