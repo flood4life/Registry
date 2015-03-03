d8fc62df0e41c52f92526f60b28455b31c8d23fc
Murad Yusufov <flood4life@gmail.com>
2015-03-03 11:42:43 +0300

    Add association between User and Review

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302150144) do
+ActiveRecord::Schema.define(version: 20150303084037) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",              default: "", null: false
@@ -49,9 +49,11 @@ ActiveRecord::Schema.define(version: 20150302150144) do
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
