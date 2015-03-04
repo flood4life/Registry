f6f3d3e484616de4be60c1c74efdaa38e9dfa913
Murad Yusufov <flood4life@gmail.com>
2015-03-04 18:31:59 +0300

    Add Bootstrap design

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'cancancan', '~> 1.10'
 
 gem 'royce'
 
+gem 'bootstrap-sass', '~> 3.3.3'
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -66,6 +66,7 @@ class ProductsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
+      @catalog = @product.catalog
     end
 
     def set_catalogs
