1c881f40df4e02af65175c1a8419199bc216ee58
Murad Yusufov <flood4life@gmail.com>
2015-03-07 18:45:34 +0300

    Add user/guest_name presence validation to Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,14 @@ class Review < ActiveRecord::Base
 
   validates :message, length: {minimum: 10}
 
+  validate :must_have_user_or_guest_name
+
+  def must_have_user_or_guest_name
+    unless guest_name.present? or user.present?
+      errors.add(:guest_name, 'is not supplied.')
+    end
+  end
+
   scope :approved, -> {where(is_pending: false)}
 
   scope :pending, -> {where(is_pending: true)}
